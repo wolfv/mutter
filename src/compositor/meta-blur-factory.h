0c2f48bc3d0035c7bcb81313e18f9ee89ab5bd7d
Wolf Vollprecht <w.vollprecht@gmail.com>
2014-10-10 18:52:13 +0200

    added gobject boilerplate code

--- src/compositor/meta-blur-factory.h
+++ src/compositor/meta-blur-factory.h
@@ -20,7 +20,10 @@ struct _MetaBlurClass {
 #define META_IS_BLUR_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), META_TYPE_BLUR))
 #define META_BLUR_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), META_TYPE_BLUR, MetaBlurClass))
 
-GType       meta_blur_get_type();
+GType meta_blur_get_type(void);
+void meta_blur_init(MetaBlur * klass) {};
+void meta_blur_class_init(MetaBlurClass * klass) {};
+MetaBlur meta_blur_new(void);
 
 void        meta_blur_paint       (MetaBlur           *shadow,
                                      int                    window_x,
